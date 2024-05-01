131f790e3da570b2a7c25dca130fe0c70452ea75
rahulonit <rahulonit@gmail.com>
2024-05-01 14:13:40 +0530

    signup complete

--- Appscreen/login.js
+++ Appscreen/login.js
@@ -29,7 +29,7 @@ export default function LoginPage({ navigation }) {
     }
     
     try {
-      const response = await fetch('http://192.168.1.2:3000/api/login', {
+      const response = await fetch('http://192.168.1.26:3000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
